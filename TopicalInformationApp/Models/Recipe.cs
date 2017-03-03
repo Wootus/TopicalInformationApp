7f93f5f5a97a61a9c0860ec9517e98a20d268567
Zachary <Zachary@SAGERMIESTER>
2017-03-03 18:48:12 -0500

    Add project files.

---
+++ TopicalInformationApp/Models/Recipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TopicalInformationApp.Models
+{
+	public class Recipe
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public double PercentABV { get; set; }
+		public string GrainAdditions { get; set; }
+		public string HopAdditions { get; set; }
+	}
+}
---
+++ TopicalInformationApp/Models/Recipes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace TopicalInformationApp.Models
+{
+	[XmlRoot("Recipes")]
+	public class Recipes
+	{
+		[XmlElement("Brewery")]
+		public List<Recipe> recipes;
+	}
+}
